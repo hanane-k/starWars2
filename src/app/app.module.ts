a979991bef6723711ea491eaaea05d09e467b8cf
hanane <nam.hana@yahoo.fr>
2019-03-21 15:58:59 +0100

    first commit (create liste component)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListesComponent } from './listes/listes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListesComponent
   ],
   imports: [
     BrowserModule,
